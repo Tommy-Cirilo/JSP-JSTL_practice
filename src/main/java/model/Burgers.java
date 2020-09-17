bf0e29b1e068c91d5346d6bab4291fca604e37ab
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-09-17 14:08:29 -0500

    add new stuff

--- src/main/java/model/Burgers.java
+++ src/main/java/model/Burgers.java
@@ -1,6 +1,7 @@
 package model;
 
 //this interface will define all the methods we need to access the data from our bean
+//This is the interace that the DAO will implement
 public interface Burgers {
     //we need a method to go find a burger by its id
     //we want the return object to be type 'Burger'
---
+++ src/main/java/model/BurgersDAO.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//This class will implement the burgers interface
+//therefore it must have all the methods defined in burgers
+public class BurgersDAO implements Burgers{
+
+    //create a temporary list, to hold the burgers as they are created
+    private List<Burgers> burgers = new ArrayList<>();
+
+    //this method will search for a burger by its id
+    //and return the full burger object
+    @Override
+    public Burger findById(long id) {
+        return burgers.get((int)id -1);
+    }
+
+    //this method will create a burger object in our local repo
+    //of burgers, and return the newly added burgers id
+    @Override
+    public long createBurger(Burger burger) {
+        burger.setId(burgers.size() + 1);
+        burgers.add(burger);
+        return 0;
+    }
+}
